8c86ff1303653f33dacca03e7883f554cd5ca3be
Ross <rosstafarian1@gmail.com>
2016-10-06 13:50:16 -0500

    Adding Application Java source

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+public class Application {
+
+    public Application() {
+        System.out.println("Inside Application");
+    }
+
+    public static void main (String[] args) {
+        System.out.println("Starting Application");
+        Application app = new Application();
+    }
+}
